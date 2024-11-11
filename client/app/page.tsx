603712d007091035ab8c84c5d77a0a335c86ee77
Jeff Lee <jeffylee@Jeffs-MacBook-Air-2.local>
2024-11-11 00:38:08 -0600

    implement SQLAlchemy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,23 +1,10 @@
-'use client'
 import Image from "next/image";
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading");
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home").then(
-      response => response.json()
-    ).then(
-      data=> {
-        setMessage(data.message);
-      }
-    );
-  }, []);
-
   return (
     <div>
-      <div>{message}</div>
+      
     </div>
   );
 }
--- server/server.py
+++ server/server.py
@@ -1,17 +1,51 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 # app instance
 app = Flask(__name__)
+app.config('SQLALCHEMY_DATABASE_URL')='sqlite:///db.sqlite'
+app.config('SQLALCHEMY_TRACK_MODIFICATIONS')=False
+db = SQLAlchemy(app)
+
 CORS(app)
 
+class Todo(db.Model):
+    # userid = db.Colum(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    done = db.Column(db.Boolean)
+
 # /api/home
-@app.route("/api/home", methods=["GET"])
-def return_home():
+@app.route('/api/home', methods=["GET"])
+def home():
+    todo_list=Todo.query.all()
     return jsonify({
-        'message': "Hello world!",
+        'todo_list': todo_list,
     })
 
+@app.route('/add', methods=['POST'])
+def add():
+    name=request.form.get("name")
+    new_task = Todo(name=name, done=False)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.get(todo_id)
+    todo.done = not todo.done
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)                     # remove debug in deployment
+    
