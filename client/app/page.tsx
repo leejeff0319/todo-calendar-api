0c27c0cceafa908d1039be172dca122cae56de20
Jeff Lee <jeffylee@Jeffs-MacBook-Air-2.local>
2024-11-11 00:17:54 -0600

    initiate Flask server and NextJS client

---
+++ client/app/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Image from "next/image";
+import React, {useEffect, useState} from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("Loading");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then(
+      data=> {
+        setMessage(data.message);
+      }
+    );
+  }, []);
+
+  return (
+    <div>
+      <div>{message}</div>
+    </div>
+  );
+}
